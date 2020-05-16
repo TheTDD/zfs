4ed0bbf71a7eb7895a0230b6823cab5199c298b2
TheTDD <the.tdd@mail.ru>
2020-05-16 16:07:37 +0200

    switch to 0.8.4

--- include/zfs_gitrev.h
+++ include/zfs_gitrev.h
@@ -1,1 +1,1 @@
-#define	ZFS_META_GITREV "unknown"
+#define	ZFS_META_GITREV "zfs-0.7.11-43-g8a423bed-dirty"
--- module/.tmp_versions/zavl.mod
+++
@@ -1,3 +0,0 @@
-/usr/local/src/zfs-0.8.3/module/avl/zavl.ko
-/usr/local/src/zfs-0.8.3/module/avl/avl.o
-
